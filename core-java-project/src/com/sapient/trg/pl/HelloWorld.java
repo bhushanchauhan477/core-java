08a7c82d5700f1267eed4bae527a240bb2477b30
bhushanchauhan477 <55916484+bhushanchauhan477@users.noreply.github.com>
2021-04-19 16:52:06 +0530

    My first commit

---
+++ core-java-project/src/com/sapient/trg/pl/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.sapient.trg.pl;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * This is java doc style comment
+ * @author bhushan
+ * This is a demo code
+ * 
+ *
+ */
+public class HelloWorld {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" Hello World ");
+		System.out.println("Today date: "+ new Date());
+		System.out.println(Calendar.getInstance());
+
+	}
+
+}
